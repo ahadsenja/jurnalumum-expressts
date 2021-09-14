895bbfcb67299dce0cabb395c7ddada2815b0d12
ahadsenja <hamdanilm96@gmail.com>
2021-09-15 04:56:50 +0800

    refactor: refactor endpoint structure operation and add some method

---
+++ src/routers/user/UserRoutes.ts
@@ -0,0 +1,23 @@
+import { Router, Request, Response } from "express";
+import IRouter from "../RouterInterface";
+
+import UserController from "../../controllers/user/UserController";
+
+class UserRoutes implements IRouter {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+    this.routes();
+  }
+
+  public routes(): void {
+    this.router.get('/', UserController.getAll);
+    this.router.get('/:id', UserController.getById);
+    this.router.post('/', UserController.create);
+    this.router.put('/:id', UserController.update);
+    this.router.delete('/:id', UserController.delete);
+  }
+}
+
+export default new UserRoutes().router;

877c105f92a490b8cf435c0db70becb9bd9c9e0c
ahadsenja <hamdanilm96@gmail.com>
2021-09-23 07:09:06 +0800

    feat: add get profile funtction

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -40,6 +40,10 @@ class AuthController {
     return res.send('Authentication Failed!');
   }
 
+  profile = (req: Request, res: Response): Response => {
+    return res.send(req.app.locals.credential);
+  }
+
 }
 
 export default new AuthController;

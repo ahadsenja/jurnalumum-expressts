d279576abd5b04613d7fd044695d56dd20eba188
ahadsenja <hamdanilm96@gmail.com>
2021-09-23 07:08:02 +0800

    refactor: create authentication middleware for request data

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,9 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 
 export const auth = (req: Request, res: Response, next: NextFunction): any => {
-  let authentication = true;
-  if (authentication) {
-    next()
+  // Check authorization in headers
+  if (!req.headers.authorization) {
+    return res.status(401).send('Token not found');
   }
-  return res.send('Unauthenticated');
+  let secretKey = process.env.JWT_SECRET_KEY || 'rahasia';
+  const token: string = req.headers.authorization.split(' ')[1];
+
+  try {
+    const credential: string | object = jwt.verify(token, secretKey);
+
+    if (credential) {
+      req.app.locals.credential = credential;
+      next()
+    }
+
+    return res.send('Invalid Token')
+  } catch (error) {
+    return res.send(error)
+  }
 }

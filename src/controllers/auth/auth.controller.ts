caa293a6d2b654d0348320fa8c907cc49b464562
ahadsenja <hamdanilm96@gmail.com>
2021-09-22 18:15:10 +0800

    feat: add password hash for encrypting the password from user

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,13 +1,27 @@
 import { Request, Response } from 'express';
 
+import PasswordHash from '../../utils/PasswordHash';
+
+const db = require('../../db/models');
+
 class AuthController {
+
+  register = async (req: Request, res: Response): Promise<Response> => {
+    let { username, password } = req.body;
+    const hashedPassword: string = await PasswordHash.hash(password);
+
+    await db.user.create({
+      username: username,
+      password: hashedPassword
+    });
+
+    return res.send('Registration success!');
+  }
+
   login(req: Request, res: Response): Response {
     return res.send('This is login endpoint');
   }
 
-  register(req: Request, res: Response): Response {
-    return res.send('This is register endpoint');
-  }
 }
 
 export default new AuthController;

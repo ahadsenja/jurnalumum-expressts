3ab7af39843fc4e74f1fd053db85ae69d6731334
ahadsenja <hamdanilm96@gmail.com>
2021-09-23 07:09:52 +0800

    feat: create endpoint profile

--- src/routers/auth/auth.routes.ts
+++ src/routers/auth/auth.routes.ts
@@ -1,5 +1,6 @@
 import BaseRoutes from '../base.routes';
 import AuthController from '../../controllers/auth/auth.controller';
+import { auth } from '../../middlewares/auth.middleware';
 
 import validate from '../../middlewares/auth.validator';
 
@@ -7,7 +8,8 @@ class AuthRoutes extends BaseRoutes {
 
   public routes() {
     this.router.post('/register', validate, AuthController.register);
-    this.router.post('/login', AuthController.login);
+    this.router.post('/login', validate, AuthController.login);
+    this.router.get('/profile', auth, AuthController.profile);
   }
 
 }

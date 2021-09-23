5583a51da7296bc3d94cbca058631d374fb26215
ahadsenja <hamdanilm96@gmail.com>
2021-09-23 09:14:26 +0800

    feat: create jurnal data

--- src/controllers/jurnal/jurnal.controller.ts
+++ src/controllers/jurnal/jurnal.controller.ts
@@ -2,6 +2,9 @@ import { Request, Response } from 'express';
 
 import IController from '../../interfaces/controller.interface';
 
+const db = require('../../db/models');
+
+
 class JurnalController implements IController {
   getAll(req: Request, res: Response): Response {
     return res.send('Get All')
@@ -11,8 +14,20 @@ class JurnalController implements IController {
     return res.send('Get by id')
   }
 
-  create(req: Request, res: Response): Response {
-    return res.send('Create')
+  create = async (req: Request, res: Response): Promise<Response> => {
+    const { id } = req.app.locals.credential;
+    const { name, description } = req.body;
+
+    const jurnal = await db.jurnal.create({
+      user_id: id,
+      name: name,
+      description: description
+    })
+
+    return res.send({
+      data: jurnal,
+      message: 'Data successfully created'
+    })
   }
 
   update(req: Request, res: Response): Response {

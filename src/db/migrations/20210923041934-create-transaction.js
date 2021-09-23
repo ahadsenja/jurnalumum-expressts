ea7f6d0d2e8d15c5fe01efc30f4d898798eebc62
ahadsenja <hamdanilm96@gmail.com>
2021-09-23 12:21:19 +0800

    feat: create migration for transaction table

---
+++ src/db/migrations/20210923041934-create-transaction.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      debit: {
+        type: Sequelize.FLOAT
+      },
+      credit: {
+        type: Sequelize.FLOAT
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('transactions');
+  }
+};

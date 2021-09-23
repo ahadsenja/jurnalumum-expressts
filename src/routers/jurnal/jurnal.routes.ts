fadb387db4122fb27f0a44cd3475702fb87010dd
ahadsenja <hamdanilm96@gmail.com>
2021-09-23 09:09:38 +0800

    refactor: add validate data payload when user input data into jurnal table

--- src/routers/jurnal/jurnal.routes.ts
+++ src/routers/jurnal/jurnal.routes.ts
@@ -1,14 +1,15 @@
 import BaseRoutes from "../base.routes";
 
 import { auth } from '../../middlewares/auth.middleware';
+import validate from "../../middlewares/jurnal.validator";
 import jurnalController from "../../controllers/jurnal/jurnal.controller";
 
 class JurnalRoutes extends BaseRoutes {
   public routes(): void {
     this.router.get('/', auth, jurnalController.getAll);
     this.router.get('/:id', auth, jurnalController.getById);
-    this.router.post('/', auth, jurnalController.create);
-    this.router.put('/:id', auth, jurnalController.update);
+    this.router.post('/', auth, validate, jurnalController.create);
+    this.router.put('/:id', auth, validate, jurnalController.update);
     this.router.delete('/:id', auth, jurnalController.delete);
   }
 }

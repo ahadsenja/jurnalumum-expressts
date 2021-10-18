16022a20b666392f5cb266a227363aafc09a46f9
ahadsenja <hamdanilm96@gmail.com>
2021-10-19 02:09:32 +0800

    reupload all files

--- build/index.js
+++ build/index.js
@@ -3,36 +3,43 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var body_parser_1 = __importDefault(require("body-parser"));
-var morgan_1 = __importDefault(require("morgan"));
-var compression_1 = __importDefault(require("compression"));
-var helmet_1 = __importDefault(require("helmet"));
-var cors_1 = __importDefault(require("cors"));
-var UserRoutes_1 = __importDefault(require("./routers/UserRoutes"));
-var App = /** @class */ (function () {
-    function App() {
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const morgan_1 = __importDefault(require("morgan"));
+const compression_1 = __importDefault(require("compression"));
+const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = require("dotenv");
+const user_routes_1 = __importDefault(require("./routers/user/user.routes"));
+const auth_routes_1 = __importDefault(require("./routers/auth/auth.routes"));
+const jurnal_routes_1 = __importDefault(require("./routers/jurnal/jurnal.routes"));
+const transaction_routes_1 = __importDefault(require("./routers/transaction/transaction.routes"));
+class App {
+    constructor() {
         this.app = (0, express_1.default)();
         this.plugins();
         this.routes();
+        (0, dotenv_1.config)();
     }
-    App.prototype.plugins = function () {
+    plugins() {
         this.app.use(body_parser_1.default.json());
         this.app.use((0, morgan_1.default)('dev'));
         this.app.use((0, compression_1.default)());
         this.app.use((0, helmet_1.default)());
         this.app.use((0, cors_1.default)());
-    };
-    App.prototype.routes = function () {
-        this.app.route('/').get(function (req, res) {
+    }
+    routes() {
+        this.app.route('/').get((req, res) => {
             res.send('Hi this test restful api');
         });
-        this.app.use('/users', UserRoutes_1.default);
-    };
-    return App;
-}());
-var port = 8000;
-var app = new App().app;
-app.listen(port, function () {
+        this.app.use('/api/v1/users', user_routes_1.default);
+        this.app.use('/api/v1/auth', auth_routes_1.default);
+        this.app.use('/api/v1/jurnals', jurnal_routes_1.default);
+        this.app.use('/api/v1/transactions', transaction_routes_1.default);
+    }
+}
+const port = 8000;
+const app = new App().app;
+app.listen(port, () => {
     console.log('This app is running on port ' + port);
 });

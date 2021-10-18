d10b790996bce619efd7203242c941e9834e377d
ahadsenja <hamdanilm96@gmail.com>
2021-10-19 02:02:25 +0800

    chore: update auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,10 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 export const auth = (req: Request, res: Response, next: NextFunction): any => {
-  // Check authorization in headers
+
   if (!req.headers.authorization) {
     return res.status(401).send('Token not found');
   }
+
   let secretKey = process.env.JWT_SECRET_KEY || 'rahasia';
   const token: string = req.headers.authorization.split(' ')[1];
 
